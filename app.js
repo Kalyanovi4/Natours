218795a5cf71f61613c6ca48de8b0b232b108c31
Kalyanovi4 <kushnir.mykolaa@gmail.com>
2023-04-26 06:45:49 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,118 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
+
+const AppError = require('./utilities/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
+const viewRouter = require('./routes/viewRoutes');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// 1) GLOBAL MIDDLEWARES
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set security HTTP headers
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        'worker-src': ['blob:'],
+        'child-src': ['blob:', 'https://js.stripe.com/'],
+        'img-src': ["'self'", 'data: image/webp'],
+        'script-src': [
+          "'self'",
+          'https://checkout.stripe.com',
+          'https://api.mapbox.com',
+          'https://cdnjs.cloudflare.com',
+          'https://js.stripe.com/v3/',
+          "'unsafe-inline'",
+        ],
+        'connect-src': [
+          "'self'",
+          'https://checkout.stripe.com',
+          'ws://localhost:*',
+          'ws://127.0.0.1:*',
+          'http://127.0.0.1:*',
+          'http://localhost:*',
+          'https://*.tiles.mapbox.com',
+          'https://api.mapbox.com',
+          'https://events.mapbox.com',
+        ],
+      },
+    },
+    crossOriginEmbedderPolicy: false,
+  })
+);
+
+// Develompents logging
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+// Limit requests from same API
+const limiter = rateLimit({
+  // 100 requests from 1 user per hour. If we need more for our APP, max number should be changed
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, pleae try again in an hour!',
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from the body in req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+
+// Data sanitization against NoSql query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parametr polution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+// Test middddleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  // console.log(req.cookies);
+  next();
+});
+
+// 2) ROUTES
+
+app.use('/', viewRouter);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
---
+++ public/js/stripe.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { showAlert } from './alerts';
+const Stripe = require('stripe');
+const stripe = Stripe(
+  'pk_test_51My5CHLxHXntS566q0cw3F3pIJnhSY38HgaCyDTT5d4Vraea20G92yt9yk4S3ob9A1lhc0SJ7FTYnVZYshlh7TDw00w0FwapNa'
+);
+
+export const bookTour = async tourID => {
+  try {
+    //Get checkout session from API
+    const data = await axios(
+      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourID}`
+    );
+
+    const redirectUrl = data.data.session.url;
+    window.location.href = redirectUrl;
+    // Create checkout form + charge credite card
+    // await stripe.redirectToCheckout({
+    //   sessionId: session.data.session.id,
+    // });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
