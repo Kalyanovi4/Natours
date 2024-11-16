5c6bba525c19e6d47d3d57e69a1375a819e61e0a
Mykola Kushnir <kushnir.mykolaa@gmail.com>
2024-11-16 14:39:50 +0200

    Commented sharp for deploying to render

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -33,11 +33,11 @@ exports.resizeTourImages = catchAsync(async (req, res, next) => {
   if (!req.files.imageCover || !req.files.images) return next();
 
   req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
-  await sharp(req.files.imageCover[0].buffer)
-    .resize(2000, 1333)
-    .toFormat('jpeg')
-    .jpeg({ quanlity: 90 })
-    .toFile(`public/img/tours/${req.body.imageCover}`);
+  // await sharp(req.files.imageCover[0].buffer)
+  //   .resize(2000, 1333)
+  //   .toFormat('jpeg')
+  //   .jpeg({ quality: 90 })
+  //   .toFile(`public/img/tours/${req.body.imageCover}`);
 
   req.body.images = [];
 
@@ -45,11 +45,11 @@ exports.resizeTourImages = catchAsync(async (req, res, next) => {
     req.files.images.map(async (file, i) => {
       const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
 
-      await sharp(file.buffer)
-        .resize(2000, 1333)
-        .toFormat('jpeg')
-        .jpeg({ quanlity: 90 })
-        .toFile(`public/img/tours/${filename}`);
+      // await sharp(file.buffer)
+      //   .resize(2000, 1333)
+      //   .toFormat('jpeg')
+      //   .jpeg({ quanlity: 90 })
+      //   .toFile(`public/img/tours/${filename}`);
 
       req.body.images.push(filename);
     })
--- controllers/userController.js
+++ controllers/userController.js
@@ -34,11 +34,11 @@ exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
 
   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
 
-  await sharp(req.file.buffer)
-    .resize(500, 500)
-    .toFormat('jpeg')
-    .jpeg({ quanlity: 90 })
-    .toFile(`public/img/users/${req.file.filename}`);
+  // await sharp(req.file.buffer)
+  //   .resize(500, 500)
+  //   .toFormat('jpeg')
+  //   .jpeg({ quanlity: 90 })
+  //   .toFile(`public/img/users/${req.file.filename}`);
 
   next();
   return;
